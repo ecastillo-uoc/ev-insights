c1a00f05ba9aaa9726131e96b47d64c61d4023de
Eva Castillo <ecastillo@uutech.es>
2026-02-12 00:31:34 +0100

    postgres access from oputside for debug

--- init.sql
+++ init.sql
@@ -20,6 +20,10 @@ CREATE SCHEMA mlflow AUTHORIZATION mlflow;
 GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow;
 GRANT ALL PRIVILEGES ON SCHEMA mlflow TO mlflow;
 
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA mlflow TO mlflow;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA mlflow TO mlflow;
+
+
 -- Connect as evinsights user to the evinsights database to create the schema and assign permissions
 \c evinsights postgres
 
@@ -29,3 +33,11 @@ CREATE SCHEMA evinsights AUTHORIZATION evinsights;
 -- Assigning permissions to the evinsights user
 GRANT ALL PRIVILEGES ON DATABASE evinsights TO evinsights;
 GRANT ALL PRIVILEGES ON SCHEMA evinsights TO evinsights;
+
+
+-- It seems that although the evinsights user has permissions on the schema, the tables (specifically Dataset) were likely created by the postgres superuser (or another user), and evinsights wasn't granted explicit permissions on those specific tables. In PostgreSQL, granting permissions on a schema does not automatically grant permissions on existing tables within it, nor on future tables created by other users unless ALTER DEFAULT PRIVILEGES is used.
+
+-- This ensures that any tables created by the postgres user in the evinsights schema will automatically be accessible to the evinsights user.
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA evinsights TO evinsights;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA evinsights TO evinsights;
